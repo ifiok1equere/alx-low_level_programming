755d8bfb74ee4cd7eb8239b74fc655004d520613
ifiok1equere <equereifiok1@gmail.com>
2022-10-14 00:58:11 +0000

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  *main finction prints the text shown
+  *The function also returns and integer o
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
