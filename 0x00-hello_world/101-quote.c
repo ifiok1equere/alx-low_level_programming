d673fc2c455daf8a4cf1470a659cd884152d9ca6
ifiok1equere <equereifiok1@gmail.com>
2022-10-14 03:33:39 +0000

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  *main - printing to sterr
+  *Description: 'Thos program prints to the standard error'
+  *Return: Function returns 1
+*/
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
