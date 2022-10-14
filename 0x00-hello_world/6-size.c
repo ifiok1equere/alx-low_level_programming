6b6f82b07a7a17afed7e55e9b62da4965b6e3cc2
ifiok1equere <equereifiok1@gmail.com>
2022-10-14 01:26:24 +0000

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  *main- Print function
+  *Description: 'The program prints the sizes of different data types
+  *Return: this function return an integer 0
+*/
+int main(void)
+{
+	printf("Size of a char: %d", sizeof(char));
+	printf("Size of an int: %d", sizeof(int));
+	printf("Size of a long int: %d", sizeof(long int));
+	printf("Size of a long long int: %d", sizeof(long long int));
+	printf("Size of a float: %d", sizeof(float));
+}
