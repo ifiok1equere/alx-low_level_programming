7ff466b2457edafbd44be2f1da3c43d050cd508d
ifiok1equere <equereifiok1@gmail.com>
2022-10-15 04:34:49 +0000

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  *main - function prints numbers
+  *Description - function prints base 10 numbers
+  *Return: returns an integer
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
