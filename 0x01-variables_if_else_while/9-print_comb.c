98155ba5e3c70164cdd15285015d5ac1bb56f653
ifiok1equere <equereifiok1@gmail.com>
2022-10-15 12:52:42 +0000

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - combo function
+ *Description - function combines integers and string literals
+ *Return: returns an integer
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 0; c < 10; c++)
+	{
+		putchar(c + '0');
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
