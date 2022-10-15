bf16c67bf1ca46d454752fa5e256d64dab61567f
ifiok1equere <equereifiok1@gmail.com>
2022-10-15 01:25:49 +0000

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,14 +6,14 @@
   *main - more headers goes there
   *Description - code checks for a positive or negative number
   *Return: returns an integer
- */
+*/
 
 int main(void)
 {
 	int n;
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
 
+	n = rand() - RAND_MAX / 2;
+	srand(time(0));
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
