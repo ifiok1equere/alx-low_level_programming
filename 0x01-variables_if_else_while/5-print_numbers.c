5ec1e8f4bad742fe703882911e36312e673492bd
ifiok1equere <equereifiok1@gmail.com>
2022-10-15 04:26:14 +0000

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - function prints lower case
+*Description - function print base 10 numbers
+*Return: returns an integer
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
