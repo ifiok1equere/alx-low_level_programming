706d6478d4b52b5ff9e5cde2543f64c00e14e039
ifiok1equere <equereifiok1@gmail.com>
2022-10-18 22:26:16 +0000

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - alphabet print
+ *Description - function prints lower case
+ *Return: returns 0
+*/
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,3 @@
 int _putchar(char c);
+int summation(void);
+void print_alphabet(void);
