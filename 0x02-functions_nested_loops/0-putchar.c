a3ac395d971e14ff5b64fd804f0ccb828731defb
ifiok1equere <equereifiok1@gmail.com>
2022-10-18 17:47:20 +0000

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  *main - call the function
+  *Description - function calls a function
+  *Return: returns an integer
+*/
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
