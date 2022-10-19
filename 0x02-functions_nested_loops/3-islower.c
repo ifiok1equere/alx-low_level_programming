354d3582885cc89687ed63c493ae8e87a0062749
ifiok1equere <equereifiok1@gmail.com>
2022-10-19 01:43:43 +0000

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+  *_islower(int c);
+  *Description - checks lowercase
+  *Return: retrns 0
+*/
+
+int _islower(int c)
+{
+	int d = 0;
+	int e = 1;
+
+	if (islower(c))
+	{
+		return e;
+	}
+	else
+	{
+		return d;
+	}
+}
+
