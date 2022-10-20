b09d3fffc3c825f021ed6bf4462d5cf7f0460587
ifiok1equere <equereifiok1@gmail.com>
2022-10-20 02:25:44 +0000

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *print_to_98 - print n numbers
+  *@n: number start
+  *Return: return int
+  */
+
+void print_to_98(int n)
+{
+	int a;
+
+	if (n > 98)
+	{
+		for (a = n; a >= 98; a--)
+		{
+			printf("%d", a);
+			if (a != 98)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
+	}
+	else if (n == 98)
+	{
+		printf("%d\n", n);
+	}
+
+	else
+	{
+		for (a = n; a <= 98; a++)
+		{
+			printf("%d", a);
+			if (a != 98)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
+	}
+}
