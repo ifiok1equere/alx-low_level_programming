5ae93e3d0c5a011a9ec027f63142d4923f51fdfc
ifiok1equere <equereifiok1@gmail.com>
2022-10-22 10:41:51 +0000

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+    *_isupper - checks for lowercase
+      *@c: character parameter
+        *Description - checks uppercase
+	  *Return: retrns 0
+	  */
+
+int _isupper(int c)
+{
+	  int d = 0, e = 1;
+
+	  if (isupper(c))
+	  {
+		  return (e);
+	  }
+	  else
+	  {
+		  return (d);
+	  }
+}
