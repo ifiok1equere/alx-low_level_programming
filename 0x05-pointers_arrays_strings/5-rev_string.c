e964fc37d8f8b3abf1e06050b40259e35da1936f
ifiok1equere <equereifiok1@gmail.com>
2022-10-26 00:12:19 +0000

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  *rev_string - reverse string
+  *@s: param
+  *Return: integer
+  */
+
+void rev_string(char *s)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	char temp;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	i--;
+
+	for (j = i; j >= i / 2; j--)
+	{
+		temp = s[k];
+		s[k] = s[j];
+		s[j] = temp;
+		k++;
+	}
+}
