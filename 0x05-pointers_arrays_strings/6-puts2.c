e6b6c1f500ede73d4933624a1adb9a486b499b9e
ifiok1equere <equereifiok1@gmail.com>
2022-10-26 00:32:20 +0000

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+   * puts2 - skipping func
+    *@str: param
+     * Return: Always nothing.
+*/
+
+
+void puts2(char *str)
+{
+	int i = 0;
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+
+	for (i = 0; i < j - 1; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
