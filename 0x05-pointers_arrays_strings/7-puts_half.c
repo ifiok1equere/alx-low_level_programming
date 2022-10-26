bae0b01771d23bc0eab75d8f679e6fd93b629582
ifiok1equere <equereifiok1@gmail.com>
2022-10-26 01:17:13 +0000

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,12 +11,12 @@ void puts_half(char *str)
 {
 	int i = 0;
 	int j;
-	
+
 	while (str[i] != '\0')
 	{
 		i++;
 	}
-	
+
 	if (i % 2 == 0)
 	{
 		for (j = i / 2; j < i; j++)
@@ -26,7 +26,7 @@ void puts_half(char *str)
 	}
 	else
 	{
-		for (j = ((i + 1) / 2); j <= i - 2; j++)
+		for (j = ((i + 1) / 2); j <= i - 1; j++)
 		{
 			_putchar(str[j]);
 		}
