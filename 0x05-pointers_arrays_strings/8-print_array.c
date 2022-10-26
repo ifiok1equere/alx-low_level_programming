60a4c0f1eecae6b5d21d8c3a534b0c4617721112
ifiok1equere <equereifiok1@gmail.com>
2022-10-26 01:59:05 +0000

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+   * print_array - comma printing
+    *@a: param1
+    *@n: param2
+    *Return: Always nothing
+*/
+
+void print_array(int *a, int n)
+{
+	int j = 0;
+
+	for (j = 0; j <= n - 1; j++)
+	{
+		printf("%d", a[j]);
+		
+		if (j < n - 1)
+		{
+			printf("%s", ",");
+			printf("%s", " ");
+		}
+	}
+
+	printf("%c", '\n');
+}
