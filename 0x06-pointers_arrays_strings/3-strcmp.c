9cc68f09a8421c917ad884d7191740ae0d46e2f0
ifiok1equere <equereifiok1@gmail.com>
2022-10-28 07:53:09 +0000

    task 4

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,7 +15,7 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0; *(s1 + i) != '\0' || *(s2 + i) != '\0'; i++)
 	{
-		if (*(s1 + i) != *(s2 + i))
+		if (*(s1 + i) - *(s2 + i) != 0)
 		{
 			l = (*(s1 + i) - *(s2 + i));
 
@@ -23,7 +23,7 @@ int _strcmp(char *s1, char *s2)
 		}
 		break;
 
-		if (*(s1 + i) == *(s2 + i))
+		if (*(s1 + i) - *(s2 + i) == 0)
 		{
 			continue;
 		}
