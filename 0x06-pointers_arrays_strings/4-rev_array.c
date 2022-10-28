cb699c5b1d8496b1922aa34a156963d11f398c00
ifiok1equere <equereifiok1@gmail.com>
2022-10-28 10:01:30 +0000

    task 5

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**reverse_array - reverse engineering
+  *@a: 1st parameter
+  *@n: 2nd parameter
+  *Return: always nothing
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i;
+	int j = 0;
+	char temp;
+
+	for (i = n - 1; i > n / 2; i--)
+	{
+		temp = a[j];
+		a[j] = a[i];
+		a[i] = temp;
+		j++;
+	}
+}
