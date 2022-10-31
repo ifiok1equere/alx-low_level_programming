276018a91fb629e8f68ac7600b6ac7557d25dbdf
ifiok1equere <equereifiok1@gmail.com>
2022-10-31 23:39:49 +0000

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,7 +13,7 @@ char *_strchr(char *s, char c)
 	char *ptr;
 	unsigned int i;
 
-	for (i = 0; i <= strlen(s) + 2; i++)
+	for (i = 0; i <= strlen(s); i++)
 	{
 		if (*(s + i) == c)
 		{
@@ -27,4 +27,5 @@ char *_strchr(char *s, char c)
 	}
 
 	return (ptr);
+	return ('\0');
 }
