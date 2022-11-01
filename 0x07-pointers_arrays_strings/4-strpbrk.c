33b5c2df6b91550bea88be74fe7ddc0a98bdc17a
ifiok1equere <equereifiok1@gmail.com>
2022-11-01 19:33:14 +0000

    task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,9 +12,9 @@ char *_strpbrk(char *s, char *accept)
 {
 	unsigned int len1 = strlen(s);
 	unsigned int len2 = strlen(accept);
-	char *ptr;
-	unsigned int i = 0;
-	unsigned int j = 0;
+	/*char *ptr;*/
+	unsigned int i;
+	unsigned int j;
 
 	for (i = 0; i <= len1; i++)
 	{
@@ -22,9 +22,9 @@ char *_strpbrk(char *s, char *accept)
 		{
 			if (*(accept + j) == *(s + i))
 			{
-				ptr = (s + i);
+				/*ptr = (s + i);*/
 
-				return (ptr);
+				return (s + i);
 			}
 			else
 			{
