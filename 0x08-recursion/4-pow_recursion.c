8e3dbc6499a51c3721112d963d15c566d112806c
ifiok1equere <equereifiok1@gmail.com>
2022-11-02 23:58:48 +0000

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  *_pow_recursion - power function
+  *@x: 1st param
+  *@y: 2nd param
+  *Return: int
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	if (y == 0)
+	{
+		return (1);
+	}
+	
+	return (x * _pow_recursion(x , y - 1));
+}
