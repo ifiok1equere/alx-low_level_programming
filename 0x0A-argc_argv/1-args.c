cd2d240d3ae80c384bd8445eb009e4e8029d0b2a
ifiok1equere <equereifiok1@gmail.com>
2022-11-05 00:25:42 +0000

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+
+/**
+  *main - argc func
+  *@argc: cml argument count
+  *@argv: pointer to cml string arguments
+  *
+  *Return: an integer
+  */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	_putchar(argc + '0');
+	_putchar('\n');
+
+	return (0);
+}
