d7472c5f34f557033bedbd0b1dd838a84d2facd9
ifiok1equere <equereifiok1@gmail.com>
2022-11-05 02:35:15 +0000

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  *main - argc func
+  *@argc: cml argument count
+  *@argv: pointer to cml string arguments
+  *
+  *Return: an integer
+  */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		char j;
+
+		j = (atoi(argv[1])) * atoi(argv[2]);
+
+		printf("%d\n", j);
+
+		return (0);
+	}
+	else
+	{
+		char *s = "Error";
+
+		printf("%s\n", s);
+
+		return (1);
+	}
+}
