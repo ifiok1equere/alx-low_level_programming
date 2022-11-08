6e27fe887b8d0ccd5ab04da8d15986403420cd21
ifiok1equere <equereifiok1@gmail.com>
2022-11-08 21:23:31 +0000

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,7 +18,7 @@ char *create_array(unsigned int size, char c)
 	}
 	else
 	{
-		ptr = (char *)malloc(sizeof(c) * size);
+		ptr = (char *)malloc(sizeof(char) * size);
 
 		for (i = 0; i < size; i++)
 		{
