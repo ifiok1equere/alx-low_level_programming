68c7113e91c2cc94164cbbc921f91cb805a372b7
ifiok1equere <equereifiok1@gmail.com>
2022-11-10 11:02:13 +0000

    task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,62 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+  *str_concat - point to a concatenated string
+  *@s1: pram is a pointer to the destination string
+  *@s2: param is a pointer to the src string
+  *Return: returns a pointer to the concatenated string
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	char *ptr;
+	unsigned int len1, len2, j;
+	unsigned int size;
+
+	if (s1 == NULL)
+	{
+		return (NULL);
+	}
+	if (s2 == NULL)
+	{
+		return (NULL);
+	}
+	
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+	size = len1 + len2;
+
+
+	/*printf("len1 = %d, len2 = %d, size = %d", len1, len2, size);*/
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	
+	ptr = (char *)malloc(sizeof(char) * size + 1);
+	
+	if (ptr != NULL)
+	{
+		for (j = 0; j < len1; j++)
+		{
+			*(ptr + j) = *(s1 + j);
+		}
+		for (j = 0; j < len2; j++)
+		{
+			*(ptr + len1 + j) = *(s2 + j);
+		}
+
+		*(ptr + size) = '\0';
+	}
+	else
+	{
+		return (NULL);
+	}
+		return (ptr);
+
+	free(ptr);
+}
