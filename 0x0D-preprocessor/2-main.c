0fffc2aa4b2ba9830ba4e62c17a1f039f41ce9ba
ifiok1equere <equereifiok1@gmail.com>
2022-11-14 20:19:46 +0000

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#ifndef _2_MAIN_C_
+#define _2_MAIN_C_
+#endif
+
+/**
+  * main - check the code
+  *
+  * Return: Always 0.
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
