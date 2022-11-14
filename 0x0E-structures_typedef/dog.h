0ac6cdfc1f9e7dd0ef918e7b511554abd66af419
ifiok1equere <equereifiok1@gmail.com>
2022-11-14 22:49:03 +0000

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+  * struct dog - func creates 3 data types
+  * @name: First member
+  * @age: Second member
+  * @owner: Third member
+  *
+  * Description: 3 datatypes created
+*/
+
+struct dog
+{
+	char *name;
+	double age;
+	char *owner;
+
+};
+
+#endif
