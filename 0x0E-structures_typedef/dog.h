0dbde78df5fb5d5ea1b80172692edaa724e99ffd
ifiok1equere <equereifiok1@gmail.com>
2022-11-15 01:05:42 +0000

    Header File

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,13 +10,13 @@
   * Description: 3 datatypes created
 */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	double age;
 	char *owner;
 
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
