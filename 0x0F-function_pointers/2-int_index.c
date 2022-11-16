09d91153af9df5fef3f0c6817a94b2ca5ffd8dd4
ifiok1equere <equereifiok1@gmail.com>
2022-11-16 15:37:39 +0000

    task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,16 +13,18 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (array == NULL || size <= 0 || cmp == NULL)
 	{
 		return (-1);
 	}
-
-	for (i = 0; i < size; i++)
+	else
 	{
-		if (cmp(array[i]))
+		for (i = 0; i < size; i++)
 		{
-			return (i);
+			if (cmp(array[i]))
+			{
+				return (i);
+			}
 		}
 	}
 
