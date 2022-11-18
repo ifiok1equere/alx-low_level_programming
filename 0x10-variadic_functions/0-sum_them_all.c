fd62d0b4b7857f06275da925b34f6e4c3d63c926
ifiok1equere <equereifiok1@gmail.com>
2022-11-18 13:52:58 +0000

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,14 +14,16 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 	va_list all_them;
 
-	va_start(all_them, n);
+	/*va_start(all_them, n);*/
 
 	if (n == 0)
 		return (0);
 
+	va_start(all_them, n);
+
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(all_them, int);
+		sum += va_arg(all_them, int)
 	}
 
 	va_end(all_them);
