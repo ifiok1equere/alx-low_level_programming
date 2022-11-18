0b3bfd71bd3aa4f806fd7d37e9df0f5b57d36c9d
ifiok1equere <equereifiok1@gmail.com>
2022-11-18 14:02:40 +0000

    task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,6 +1,6 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
-#include <stdlib.h>
+#include <stdio.h>
 
 /**
   *sum_them_all - summing function
@@ -14,16 +14,14 @@ int sum_them_all(const unsigned int n, ...)
 	int sum = 0;
 	va_list all_them;
 
-	/*va_start(all_them, n);*/
+	va_start(all_them, n);
 
 	if (n == 0)
 		return (0);
 
-	va_start(all_them, n);
-
 	for (i = 0; i < n; i++)
 	{
-		sum += va_arg(all_them, int)
+		sum += va_arg(all_them, int);
 	}
 
 	va_end(all_them);
