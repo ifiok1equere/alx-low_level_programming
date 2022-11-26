82391f6cacab80e5c0ea74917509e4d477328c97
ifiok1equere <equereifiok1@gmail.com>
2022-11-26 20:59:06 +0000

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,21 +10,22 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t count;
-	const list_t *ptr;
+	size_t count = 0;
 
-	ptr = h;
-	
-	if(ptr->str == NULL)
-	{
-		printf("[0] (nil)\n");
-	}
 
-	while(ptr->next != NULL)
+	while(h)
 	{
+		if(h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+
+		h = h->next;
 		count++;
-		printf("[%d] %s\n", h->len, h->str);
-		ptr = ptr->next;
 	}
 
 	return (count);
