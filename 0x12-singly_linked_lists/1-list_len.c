fb6a05ea1cd3a040082f6ae3b145c73ceaa205b3
ifiok1equere <equereifiok1@gmail.com>
2022-11-26 21:18:13 +0000

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  *print_list - print element function
+  *@h: node
+  *Return: unsigned integer
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
