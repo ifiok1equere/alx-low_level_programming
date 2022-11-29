f3a1dfcc4d9076a764e7261e37c181e73c7a4ab4
ifiok1equere <equereifiok1@gmail.com>
2022-11-29 20:06:50 +0000

    task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,32 +8,18 @@
   *@head: pointer to list.
   *Return: returns nothing
 */
-
 void free_list(list_t *head)
 {
 	list_t *ptr;
-	/*list_t *p;*/
 
-	ptr = head;
-
-	if (ptr == NULL)
+	while (head)
 	{
-		return;
-	}
-	else
-	{
-		while (ptr != NULL)
-		{
-			ptr = head;
-			head = head->next;
-			free(ptr->str);
-			free(ptr);
-		}
-	}
-		
+		if (head->str)
+			free(head->str);
 
-	free(head);
-	head = NULL;
-	
-	return;
+		ptr = head;
+		head = head->next;
+
+		free(ptr);
+	}
 }
