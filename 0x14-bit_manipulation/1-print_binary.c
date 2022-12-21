728af12da345cec668fe8e8460adf98a9332cb45
ifiok1equere <equereifiok1@gmail.com>
2022-12-21 21:07:41 +0000

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+  * print_binary - conversion function
+  * @n: decimal number to be converted
+  * Return: returns nothing
+*/
+
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int dec_num, bin_num, num, rem;
+
+	num = 1;
+	bin_num = 0;
+	
+	if (n == 0)
+	{
+		printf("%lu", n);
+		return;
+	}
+
+	while (n > 0)
+	{
+		dec_num = n >> 1;
+		rem = n - (2 * dec_num);
+		bin_num = bin_num + (rem * num);
+		num = num * 10;
+
+		n = n >> 1;
+	}
+
+	printf("%lu", bin_num);
+}
