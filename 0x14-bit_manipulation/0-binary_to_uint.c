bda98f7c71c93e0fe69eedfa497146921409bb02
ifiok1equere <equereifiok1@gmail.com>
2022-12-21 10:34:54 +0000

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,34 +6,25 @@
 /**
   * binary_to_uint - conversion function
   * @b: pointer argument to string
-  *
   * Return: rerturns an unsigned integer
 */
-
 unsigned int binary_to_uint(const char *b)
 {
-	/*unsigned int index, base, dec, i, j, r, bin_num, binary;*/
-		
-	/*base = 1;*/
-	unsigned int dec, bin_num;
-	unsigned char bit_num;
-	unsigned int j, k, index;
+	unsigned int dec, bin_num, bit_num, j, k, index;
 	int i, l;
-	
-	dec = 0;
-	index = i = j = k = l = 0;
+
+	dec = index = i = j = k = l = 0;
 
 	if (b == NULL)
 		return (0);
 
 	bin_num = atoi(b);
-	
+
 	while (bin_num > 0)
 	{
 		k++;
 		bin_num = bin_num / 10;
 	}
-	
 	while (b[j] != '\0')
 	{
 		if (b[j] == '1')
@@ -41,19 +32,15 @@ unsigned int binary_to_uint(const char *b)
 			index = j;
 			break;
 		}
-
 		j++;
 	}
-	
 	if (strlen(b) - index != k)
 		return (0);
-
 	for (i = strlen(b) - 1; i >= 0; i--)
 	{
 		bit_num = b[i] - '0';
 		dec = dec + (bit_num << l);
 		l++;
 	}
-	
 	return (dec);
 }
