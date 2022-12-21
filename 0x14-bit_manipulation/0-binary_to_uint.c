618668138e6088ae5e1a4e59b39d5bfd1b593b54
ifiok1equere <equereifiok1@gmail.com>
2022-12-21 13:54:34 +0000

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,37 +10,28 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int dec, bin_num, bit_num, j, k, index;
-	int i, l;
+	unsigned int dec, bit_num, j;
+	int i;
 
-	dec = index = i = j = k = l = 0;
+	dec = i = j =0;
 
 	if (b == NULL)
 		return (0);
 
-	bin_num = atoi(b);
-
-	while (bin_num > 0)
-	{
-		k++;
-		bin_num = bin_num / 10;
-	}
-	while (b[j] != '\0')
-	{
-		if (b[j] == '1')
-		{
-			index = j;
-			break;
-		}
-		j++;
-	}
-	if (strlen(b) - index != k)
-		return (0);
 	for (i = strlen(b) - 1; i >= 0; i--)
 	{
 		bit_num = b[i] - '0';
-		dec = dec + (bit_num << l);
-		l++;
+
+		if (bit_num > 9)
+		{
+			return(0);
+		}
+		else
+		{
+			dec = dec + (bit_num << j);
+			j++;
+		}
 	}
+
 	return (dec);
 }
