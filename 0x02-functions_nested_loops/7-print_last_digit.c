6fb4340197f7814dfd7e23669814c77fcb5537e1
ifiok1equere <equereifiok1@gmail.com>
2023-02-21 18:35:13 +0100

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function prints last digit of a number
+ * @n: integer function parameter
+ *
+ * Return: returns interger
+ */
+int _abs(int n);
+int print_last_digit(int n)
+{
+	int m;
+	
+	if (n < 0)
+	{
+		m = _abs(n);
+		m = m % 10;
+		_putchar(m + '0');
+		return (m);
+	}
+	else if (n == 0)
+	{
+		_putchar(0 + '0');
+		return (0);
+	}
+	else
+	{
+		m = n % 10;
+		_putchar(m + '0');
+		return (m);
+	}
+}
+
+
+/**
+ * _abs - function computes the absolute value of a number
+ * @n: integer parameter whose absolute value is to be computed
+ *
+ * Return: returns interger
+ */
+
+int _abs(int n)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+	else if (n < 0)
+	{
+		n = n * -1;
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
