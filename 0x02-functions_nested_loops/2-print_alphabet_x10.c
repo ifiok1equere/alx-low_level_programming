3d30c77310be72f3ade505966a75d0e624d7c412
ifiok1equere <equereifiok1@gmail.com>
2023-02-21 13:46:51 +0100

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function prints lowercase alphabets in reverse
+ * @void: no parameters needed for function output
+ *
+ * Return: returns void
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0, j;
+
+	while (i <= 10)
+	{
+		j = 'a';
+		while (j <= 'z')
+		{
+			_putchar(j);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
