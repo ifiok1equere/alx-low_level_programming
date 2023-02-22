e47a501e4d11462b94162246c30b8cec4a9297fb
ifiok1equere <equereifiok1@gmail.com>
2023-02-22 01:27:48 +0100

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * times_table - function prints the 9-times table
+ *
+ * Return: nothing as a void function.
+ */
+void times_table(void)
+{
+	char i = 0, j = 0, k;
+
+	while (i <= 9)
+	{
+		while (j <= 9)
+		{
+			k = i * j;
+			if (k <= 9)
+			{
+				if ((j < 9) && ((i * (j + 1))) <= 9)
+				{
+					_putchar(k + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar(k + '0');
+					if (j != 9)
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
+				}
+			}
+			else
+			{
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+				if (j < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			j++;
+		}
+		_putchar('\n');
+		j = 0;
+		i++;
+	}
+}
