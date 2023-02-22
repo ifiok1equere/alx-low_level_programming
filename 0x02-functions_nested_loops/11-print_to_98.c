dd9d775349d49b0d35d76e5905e48dd867ca5f28
ifiok1equere <equereifiok1@gmail.com>
2023-02-22 14:35:36 +0100

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,37 +8,19 @@
  */
 void print_to_98(int n)
 {
-	if (n >= 0)
+	if (n > 98)
 	{
-		if (n > 98)
+		while (n >= 98)
 		{
-			while (n >= 98)
+			if (n != 98)
 			{
-				if (n != 98)
-				{
-					printf("%d, ", n);
-				}
-				else
-				{
-					printf("%d\n", n);
-				}
-				n--;
+				printf("%d, ", n);
 			}
-		}
-		else
-		{
-			while (n <= 98)
+			else
 			{
-				if (n != 98)
-				{
-					printf("%d, ", n);
-				}
-				else
-				{
-					printf("%d\n", n);
-				}
-				n++;
+				printf("%d\n", n);
 			}
+			n--;
 		}
 	}
 	else
