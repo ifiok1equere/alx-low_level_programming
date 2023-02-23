40787276b9687e561a727cea6934b057dbfa0597
ifiok1equere <equereifiok1@gmail.com>
2023-02-23 21:28:58 +0100

    Header File

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,5 +5,7 @@
 
 void positive_or_negative(int i);
 long int largest_number(long int a, long int b, long int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
