b95397bbf0dfe66f520a34cd8e52d585caa14e09
ifiok1equere <equereifiok1@gmail.com>
2023-02-23 01:49:51 +0100

    advanced task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -15,16 +15,16 @@ int main(void)
 }
 
 /**
- * fibonacci_98 - function prints the first 50 Fibonacci numbers.
+ * fibonacci_98 - function prints the first 98 Fibonacci numbers.
  *
  * Return: void
  */
 void fibonacci_98(void)
 {
-	long prev = 1, cur = 2, next, i = 1;
+	unsigned long int prev = 1, cur = 2, next = 0, i = 1;
 
 	next = prev + cur;
-	printf("%ld, %ld, %ld, ", prev, cur, next);
+	printf("%lu, %lu, %lu, ", prev, cur, next);
 
 	while (i < 96)
 	{
@@ -33,11 +33,11 @@ void fibonacci_98(void)
 		next = prev + cur;
 		if (i < 95)
 		{
-			printf("%ld, ", next);
+			printf("%lu, ", next);
 		}
 		else
 		{
-			printf("%ld\n", next);
+			printf("%lu\n", next);
 		}
 		i++;
 	}
