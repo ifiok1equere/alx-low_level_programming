25b8d09155316b2d185ed2f1486cc005379adc40
ifiok1equere <equereifiok1@gmail.com>
2023-02-23 01:22:22 +0100

    advanced task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+void fibonacci_even_sum(void);
+
+int main(void)
+{
+	fibonacci_even_sum();
+	return (0);
+}
+
+/**
+ * fibonacci_even_sum - function computes sum of even numbers in f.series.
+ *
+ * Return: void
+ */
+void fibonacci_even_sum(void)
+{
+	long prev = 1, cur = 2, next, even, max = 4000000;
+
+	printf("%ld, %ld, ", prev, cur);
+	next = prev + cur;
+	even = cur;
+
+	while (next < max)
+	{
+		prev = cur;
+		cur = next;
+		next = prev + cur;
+		if (next % 2 == 0 && next < max)
+		{
+			even += next;
+			printf("%ld, ", even);
+		}
+	}
+	printf("\n");
+}
