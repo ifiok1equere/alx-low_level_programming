97de5596d4b6e343e2d451836cbea519079640ee
ifiok1equere <equereifiok1@gmail.com>
2023-02-23 23:58:35 +0100

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - function prints numbers from 0 to 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i  <= 9)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}
