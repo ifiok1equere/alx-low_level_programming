19d4a91f061ae4e4dfc6bd93aa5bfefe33bcb44d
ifiok1equere <equereifiok1@gmail.com>
2023-02-23 23:47:26 +0100

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - function multiplies 2 integers
+ * @a: 1st operand
+ * @b: 2nd operand
+ *
+ * Return: integer for success
+ */
+int mul(int a, int b)
+{
+	long int c;
+
+	c = a * b;
+	return (c);
+}
