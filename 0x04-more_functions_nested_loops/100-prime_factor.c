8919f4c34c8852e8e5ed80c758e1057286263300
ifiok1equere <equereifiok1@gmail.com>
2023-02-24 04:47:18 +0100

    Advanced task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * prime_factor - function prints the largest prime factor of a n
+ * @n: number whose largest prime factor is to be found
+ *
+ * Return: 0 for success
+ */
+void prime_factor(long int n)
+{
+	long int i = 2, max = 0;
+
+	while (i)
+	{
+		if (n % i != 0)
+		{
+			i++;
+		}
+		else
+		{
+			max = n;
+			n = n / i;
+			if (n == 1)
+			{
+				printf("%ld\n", max);
+				break;
+			}
+		}
+	}
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    prime_factor(612852475143);
+    return (0);
+}
