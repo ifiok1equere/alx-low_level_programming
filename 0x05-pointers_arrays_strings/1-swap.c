19471540458482530d72909875e887901b9a2aa9
ifiok1equere <equereifiok1@gmail.com>
2023-02-27 22:54:32 +0100

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * swap_int - function swaps the values of to arguments
+ *
+ * @a: pointer to integer variable
+ * @b: pointer to integer variable
+ *
+ * Return: void
+ */
 void swap_int(int *a, int *b)
 {
 	int temp;
