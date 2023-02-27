1e845dab5b0c3ae668048f2ed1c9486f68834c48
ifiok1equere <equereifiok1@gmail.com>
2023-02-27 22:52:34 +0100

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * reset_to_98 - function updates the value of a variable using pointers
+ *
+ * @n: parameter pointing to the variable whose value is to be chnaged.
+ *
+ * Return: void
+ */
+
 void reset_to_98(int *n)
 {
 	*n = 98;
