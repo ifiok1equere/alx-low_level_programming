8c69fffd130607d399d7133e01356b10da76d2df
ifiok1equere <equereifiok1@gmail.com>
2023-02-28 01:29:32 +0100

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * puts2 - function prints every other character of a string.
+ *
+ * @str: pointer to type char string variable to be printed.
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0, len = 0;
+
+	len = strlen(str);
+
+	while (i <= len)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
