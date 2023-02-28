ad9499049747b087cb8ac78ef0f752e801816032
ifiok1equere <equereifiok1@gmail.com>
2023-02-28 01:45:38 +0100

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts_half - function prints half of a string.
+ *
+ * @str: pointer to type char string variable to be printed.
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int n = 0, len = 0;
+
+	len = strlen(str);
+	if (len % 2 == 0)
+	{
+		n = len / 2;
+	}
+	else
+	{
+		n = (len - 1) / 2;
+	}
+
+	while (n < len)
+	{
+		_putchar(str[n]);
+		n++;
+	}
+	_putchar('\n');
+}
