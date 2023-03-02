afd6c7d5af56f2fcd3f3bdf982e69c24f7606604
ifiok1equere <equereifiok1@gmail.com>
2023-03-03 00:31:49 +0100

    Advanced task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_number - function prints an integer.
+ *
+ * @n: integer to be printed
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+
+	_putchar((n % 10) + '0');
+}
