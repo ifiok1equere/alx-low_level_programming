c12594ef39488e42f5efdea241b70c0cf471ca6b
ifiok1equere <equereifiok1@gmail.com>
2023-03-07 04:10:45 +0100

    Advanced task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_string - function prints the sum of the 2 diagonals of a sqaure
+ * matrix.
+ *
+ * @s: double pointer to char.
+ * @to: pointer to char.
+ *
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+
+}
