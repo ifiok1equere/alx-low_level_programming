75ea0cc63e9c6be36ebf334e4cc53aef0d2acd20
ifiok1equere <equereifiok1@gmail.com>
2023-03-09 01:21:28 +0100

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -11,12 +11,13 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	while (s[i])
+	if (*s == '\0')
 	{
-		_putchar(s[i]);
-		i++;
+		_putchar('\n');
+		return;
 	}
-	_putchar('\n');
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+
 }
