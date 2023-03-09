1e870488bcd7522502784a055cf0c03b14532ade
ifiok1equere <equereifiok1@gmail.com>
2023-03-09 05:21:13 +0100

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -18,18 +18,18 @@ int increment(int, int);
 
 int _sqrt_recursion(int n)
 {
-        int m = 1;
+	int m = 1;
 
-        if (n == 0)
-        {
-                return (0);
-        }
-        if (n == 1)
-        {
-                return (1);
-        }
+	if (n == 0)
+	{
+		return (0);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
 
-        return (increment(m, n));
+	return (increment(m, n));
 }
 
 /**
@@ -43,19 +43,19 @@ int _sqrt_recursion(int n)
 
 int increment(int m, int n)
 {
-        if (m * m == n)
-        {
-                return (m);
-        }
-        else
-        {
-                if (m <= n / 2)
-                {
-                        return (increment(m + 1, n));
-                }
-                else
-                {
-                        return (-1);
-                }
-        }
+	if (m * m == n)
+	{
+		return (m);
+	}
+	else
+	{
+		if (m <= n / 2)
+		{
+			return (increment(m + 1, n));
+		}
+		else
+		{
+			return (-1);
+		}
+	}
 }
