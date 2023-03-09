d2353c344d8496f1bcfcf8f9da0c287bab433179
ifiok1equere <equereifiok1@gmail.com>
2023-03-09 05:31:09 +0100

    Advanced task 8

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  *reverse_check - reverse checker
+  *@s: pointer argument to string literal
+  *@i: 1st increment parameter
+  *@j: 2nd increment parameter
+  *Return: returns an integer
+  */
+
+int reverse_check(char *s, int i, int j)
+{
+	if (*(s + j) == *(s + i))
+	{
+		if (j <= (i / 2))
+		{
+			return (reverse_check(s, --i, ++j));
+		}
+		else
+		{
+			return (1);
+		}
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+/**
+  *is_palindrome - reverse check function
+  *@s: pointer argument to string literal
+  *Return: returns an integer
+*/
+
+int wildcmp(char *s1, char *s2)
+{
+	int i;
+	int j;
+	int k = 0;
+
+	i = strlen(s1) - 1;
+	j = strlen(s2) - 1;
+
+	if (*(s + k) != *(s + i))
+	{
+		return (0);
+	}
+	else
+	{
+		return (reverse_check(s, i, j));
+	}
+}
