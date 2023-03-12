e222d5518598726fff41cf3d398191d36f9ee6c2
ifiok1equere <equereifiok1@gmail.com>
2023-03-12 12:13:00 +0100

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -21,14 +21,14 @@ int main(int argc, char *argv[])
 	}
 	while (i < argc)
 	{
-
 		if (*argv[i] >= 48 && *argv[i] <= 57)
 		{
 			add += atoi(argv[i]);
 		}
 		else if (atoi(argv[i]) < 0)
 		{
-			add += (atoi(argv[i]));
+			printf("%d\n", atoi(argv[i - 1]));
+			return (0);
 		}
 		else
 		{
