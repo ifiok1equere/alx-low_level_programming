76866f0a112995fe81a42393d21ac207cb34731f
ifiok1equere <equereifiok1@gmail.com>
2023-03-13 13:23:53 +0100

    task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - function returns a pointer to a the copy of a string pointer to by
+ * parameter str.
+ * @str: pointer to the string to be copied.
+ *
+ * Return: a pointer to the newly copied string.
+ */
+char *_strdup(char *str)
+{
+	char *ptr;
+	unsigned int i = 0, len = 0;
+
+	if (str == NULL)
+		return (NULL);
+
+	len = strlen(str);
+	ptr = (char *)malloc(sizeof(char) * len);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	while (i < len)
+	{
+		ptr[i] = str[i];
+		i++;
+	}
+
+	return (ptr);
+}
