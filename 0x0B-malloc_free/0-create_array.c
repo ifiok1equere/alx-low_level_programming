03ddd706a5871aaeef5d42e1d87f7f8d227646ae
ifiok1equere <equereifiok1@gmail.com>
2023-03-13 13:08:27 +0100

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,17 +11,18 @@
  */
 char *create_array(unsigned int size, char c)
 {
-    char *ptr;
-    unsigned int i = 0;
+	char *ptr;
+	unsigned int i = 0;
 
-    ptr = (char *)malloc(sizeof(char) * size);
-    if (ptr == NULL || size == 0)
-	    return (NULL);
+	ptr = (char *)malloc(sizeof(char) * size);
+	if (ptr == NULL || size == 0)
+		return (NULL);
 
-    while (i < size)
-    {
-	    ptr[i] = c;
-	    i++;
-    }
-    return (ptr);
+	while (i < size)
+	{
+		ptr[i] = c;
+		i++;
+	}
+
+	return (ptr);
 }
