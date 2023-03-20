5a471bcc8788dbe7f33d863c83b20c34194f5b8c
ifiok1equere <equereifiok1@gmail.com>
2023-03-20 21:53:57 +0100

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,8 +15,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *ptr;
 	dog_t p;
 
-	if ((name == NULL) | (owner == NULL))
-		return (NULL);
+	/*if ((name == NULL) | (owner == NULL))
+		return (NULL);*/
 
 	p.name = name;
 	p.age = age;
