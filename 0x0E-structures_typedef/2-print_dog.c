5e7b6d28ecaa75fba80923c5f363334ef0f93393
ifiok1equere <equereifiok1@gmail.com>
2023-03-20 22:39:39 +0100

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,24 +15,18 @@ void print_dog(struct dog *d)
 		return;
 	if (d->name == NULL)
 	{
-		printf("Name: (nil)\n");
+		printf("%s\n", "Name: (nil)");
 	}
 	else
 	{
 		printf("Name: %s\n", d->name);
 	}
-	if (d->age == 0.0)
-	{
-		printf("Age: (nil)\n");
-	}
-	else
-	{
-		printf("Age: %f\n", d->age);
-	}
+
+	printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
 	{
-		printf("Owner: (nil)\n");
+		printf("%s\n", "Owner: (nil)");
 	}
 	else
 	{
