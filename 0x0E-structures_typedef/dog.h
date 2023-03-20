83003f25163c92704b14c52071cdea4aaa55db28
ifiok1equere <equereifiok1@gmail.com>
2023-03-20 19:51:09 +0100

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
 #ifndef DOG_
 #define DOG_
 
+/**
+ * struct dog - User defined data type consisting of 3 primitive types
+ * @name: element refers to name of the object with type pointer to char
+ * @age: element refers to the object age with type float
+ * @owner: element refers to object with type pointer to char
+ *
+ * Description: this structure declaration creates a multi-element object with
+ * name, age and owner of types pointer to char, float and pointer to char
+ * respectively
+ */
+
 struct dog
 {
 	char *name;
