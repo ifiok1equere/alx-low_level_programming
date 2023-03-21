e72b9f6b882bc9681eedb555b3f794c70e06ea96
ifiok1equere <equereifiok1@gmail.com>
2023-03-21 10:32:01 +0100

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,20 +19,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if ((name == NULL) | (owner == NULL))
 		return (NULL);
 	ptr = malloc(sizeof(dog_t));
-	if (ptr == NULL)
-		return (NULL);
 	ptr->name = malloc(sizeof(strlen(name) + 1));
-	if (ptr->name == NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
 	ptr->owner = malloc(sizeof(strlen(owner) + 1));
-	if (ptr->owner == NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
 	if (ptr->name && ptr->owner)
 	{
 		for (i = 0; i < strlen(name); i++)
