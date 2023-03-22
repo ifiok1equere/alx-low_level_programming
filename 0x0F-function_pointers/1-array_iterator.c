fa05824ea6434933bc12c7d23c18cd4eac18f808
ifiok1equere <equereifiok1@gmail.com>
2023-03-22 15:58:36 +0100

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * array_iterator - function executes another funtion
+ * @array: 1st parameter
+ * @size: 2nd parameter
+ * @action: 3rd parameter
+ *
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i =0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
