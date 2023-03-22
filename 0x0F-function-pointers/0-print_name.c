24cc35a09d2b2651769ca3255a5f855020690475
ifiok1equere <equereifiok1@gmail.com>
2023-03-22 15:28:45 +0100

    task 0

--- 0x0F-function-pointers/0-print_name.c
+++ 0x0F-function-pointers/0-print_name.c
@@ -1,8 +1,10 @@
 #include "function_pointers.h"
+
 /**
  * print_name - function prints name
  * @name: 1st parameter of function which is the name to be printed
  * @f: 2nd parametr of function which is a function pointer.
+ *
  * Return: void
  */
 
