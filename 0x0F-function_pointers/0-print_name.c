1c553566783b155fb5fe4ea5a6be3cc55ab78a63
ifiok1equere <equereifiok1@gmail.com>
2023-03-22 15:49:11 +0100

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * print_name - function prints name
@@ -10,5 +11,12 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	else
+	{
+		f(name);
+	}
 }
