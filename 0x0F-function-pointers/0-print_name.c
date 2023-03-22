3e222c0177795e6f6b5f8432f7b1e5d05a6d2f10
ifiok1equere <equereifiok1@gmail.com>
2023-03-22 15:17:17 +0100

    task 0

---
+++ 0x0F-function-pointers/0-print_name.c
@@ -0,0 +1,11 @@
+/**
+ * print_name - function prints name
+ * @name: 1st parameter of function which is the name to be printed
+ * @f: 2nd parametr of function which is a function pointer.
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
