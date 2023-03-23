2295ab955611aa75495abf0425de01942225637a
ifiok1equere <equereifiok1@gmail.com>
2023-03-23 03:47:02 +0100

    Advanced task 4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program prints the opcodes of this main function.
+ * @argc: CLI argument count
+ * @argv: array of CLI strings
+ *
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	char *ptr;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	ptr = argv[0];
+
+	for (i = 0; i < atoi(argv[1]); i++)
+	{
+		printf("%02hhx ", ptr[i]);
+	}
+	printf("\n");
+
+	return (0);
+}
