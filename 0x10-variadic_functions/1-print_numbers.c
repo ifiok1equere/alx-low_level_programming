19400e37af5a4c14d1a76d115dc362249eb6faee
ifiok1equere <equereifiok1@gmail.com>
2023-03-23 21:32:53 +0100

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,12 +21,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(arg, n);
 
-	for (i = 0; i < (n - 1); i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(arg, int));
-		if (separator != NULL)
+		if ((separator != NULL) && (i != (n - 1)))
 			printf("%s", separator);
 	}
-	printf("%d\n", va_arg(arg, int));
 	va_end(arg);
 }
