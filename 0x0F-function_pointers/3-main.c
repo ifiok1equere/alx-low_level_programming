7ab8f27b87e387944f493f4542f8c6cfde57ce8f
ifiok1equere <equereifiok1@gmail.com>
2023-03-23 02:22:11 +0100

    main.c file

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	/*int i, j = 0*/
+	int final_int_return;
+	/*char *array[] = {"+", "-", "*", "/", "%"};*/
+	int (*func_pointer)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	else if (strlen(argv[2]) > 1)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (strcmp(argv[3], "0") == 0)
+	{
+		if ((strcmp(argv[2], "/") == 0) || (strcmp(argv[2], "%") == 0))
+		{
+			printf("Error\n");
+			exit(100);
+		}
+	}
+	func_pointer = get_op_func(argv[2]);
+
+	func_pointer = get_op_func(argv[2]);
+	final_int_return = func_pointer(atoi(argv[1]), atoi(argv[3]));
+
+	printf("%d\n", final_int_return);
+
+	return (0);
+}
