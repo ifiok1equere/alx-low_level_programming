341c7b0bc31cbba23496f55bd1f2e0e75a04adf0
ifiok1equere <equereifiok1@gmail.com>
2023-03-23 02:23:27 +0100

    opns functions file

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - function adds two numbers
+ * @a: 1st integer
+ * @b: 2nd integer
+ *
+ * Return: integer
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function subtract two numbers
+ * @a: 1st integer
+ * @b: 2nd integer
+ *
+ * Return: integer
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function multiplies two numbers
+ * @a: 1st integer
+ * @b: 2nd integer
+ *
+ * Return: integer
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function divides two numbers
+ * @a: 1st integer
+ * @b: 2nd integer
+ *
+ * Return: integer
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - function gets the mod of two numbers
+ * @a: 1st integer
+ * @b: 2nd integer
+ *
+ * Return: integer
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
