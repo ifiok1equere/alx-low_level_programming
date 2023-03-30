b56bc7405d2c7588f9446c1303a407af59779c05
ifiok1equere <equereifiok1@gmail.com>
2023-03-30 22:54:59 +0100

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -19,12 +19,14 @@ size_t print_list(const list_t *h)
 	}
 	while (h != NULL)
 	{
-		if (h->str != NULL)
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
 		{
 			printf("[%d] %s\n", h->len, h->str);
 		}
-		else
-			printf("[0] (nil)\n");
 		h = h->next;
 		i++;
 	}
