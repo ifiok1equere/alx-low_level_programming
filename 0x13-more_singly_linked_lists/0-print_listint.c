460f77d3f645ab52ed70fd33596a7463f914a454
ifiok1equere <equereifiok1@gmail.com>
2023-04-03 22:27:44 +0100

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint - function prints all elemnts of a linked list
+ * @h: pointer to the head node of the list
+ *
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *ptr;
+	size_t i = 0;
+
+	if(h == NULL)
+		return (1);
+
+	for(ptr = h; ptr != NULL; ptr = ptr->next)
+	{
+		printf("%d\n", ptr->n);
+		i++;
+	}
+	return (i);
+}
