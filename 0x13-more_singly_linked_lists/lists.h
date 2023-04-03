1d5d116ba0e2172a1d0ccdc16232ea6fc79c7af4
ifiok1equere <equereifiok1@gmail.com>
2023-04-03 22:33:11 +0100

    Header File

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,3 +1,5 @@
+#ifndef LISTS_H
+#define LISTS_H
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -22,3 +24,4 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+#endif
