c1bb75930845d8bf58a319c5431e7926c48d4d1e
ifiok1equere <equereifiok1@gmail.com>
2023-04-05 00:53:04 +0100

    task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,11 +9,15 @@
  */
 int pop_listint(listint_t **head)
 {
+	listint_t *ptr;
 	int n;
 
 	if (head == NULL || *head == NULL)
 		return (0);
 	n = (*head)->n;
+	ptr = *head;
 	*head = (*head)->next;
+	free(ptr);
+
 	return (n);
 }
