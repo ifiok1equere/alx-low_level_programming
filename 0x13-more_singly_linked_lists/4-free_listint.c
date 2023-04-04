1bf98180f81a2f07fd9be70437c9f77997902ea4
ifiok1equere <equereifiok1@gmail.com>
2023-04-04 23:30:00 +0100

    task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - function frees a list
+ * @head: pointer to the head node of the list
+ *
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *ptr;
+
+	if (head->next == NULL)
+	{
+		free(head);
+		head = NULL;
+		return;
+	}
+	ptr = head;
+
+	while (ptr != NULL)
+	{
+		head = head->next;
+		free(ptr);
+		ptr = head;
+	}
+}
