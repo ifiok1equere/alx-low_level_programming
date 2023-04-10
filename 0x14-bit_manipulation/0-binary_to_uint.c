6af3b71f62ac924657c4cab65b06493fbf1c2a2e
ifiok1equere <equereifiok1@gmail.com>
2023-04-10 15:51:05 +0100

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,7 +15,7 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	while (b[i] != '\0')
 	{
-		if (b[i] != '0' || b[i] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		i++;
 	}
