27959ef77bddb52f998fec5b69af770f5726af8d
ifiok1equere <equereifiok1@gmail.com>
2023-05-18 12:13:16 +0100

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * print_dlistint -  function that prints all the elements of a list.
- * @h: pointer to the head node of the list
+ * @h: pointer to the head node of the linked list
  * Return: number of nodes in the list
  */
 size_t print_dlistint(const dlistint_t *h)
