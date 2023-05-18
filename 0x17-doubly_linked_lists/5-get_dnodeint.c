dee2d26969ad2ab3ad1c5a6eabe22241ec95475d
ifiok1equere <equereifiok1@gmail.com>
2023-05-18 12:14:47 +0100

    task 5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - searches for the nth node of a list.
+ * @head: pointer to the head node of the list
+ * @index: position of node to be returned
+ * Return: nth node of a list
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *ptr;
+	unsigned int i = 0;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	if (i > index)
+		return (NULL);
+	ptr = head;
+	while (i < index)
+	{
+		ptr = ptr->next;
+		i++;
+	}
+
+	return (ptr);
+}
