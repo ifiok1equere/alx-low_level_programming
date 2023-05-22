d24d4e96007a5b670a6ecdd189674c46fd92b40c
ifiok1equere <equereifiok1@gmail.com>
2023-05-22 17:15:49 +0100

    0x18. C - Dynamic libraries

---
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stddef.h>
+#include <string.h>
+
+/**
+ * _strchr - function locates a character in string.
+ *
+ * @s: pointer to string in which charcter search is to be made.
+ * @c: character to be searched for.
+ *
+ * Return: pointer to first occurence of c in the string s or null is c is not
+ * found.
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; i <= strlen(s); i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+	return (NULL);
+}
