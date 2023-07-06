391447f0d68267236176226fdfc5d9c62414e9fe
ifiok1equere <equereifiok1@gmail.com>
2023-07-06 15:31:01 +0100

    task 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create- Function to create a hash table
+ *
+ * @size: The size of the array
+ * Return: Pointer to the hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	unsigned long int i;
+
+	table = malloc(sizeof(hash_table_t));
+	table->size = size;
+	table->array = calloc(table->size, sizeof(hash_node_t*));
+
+	for (i = 0; i < table->size; i++)
+		table->array[i] = NULL;
+
+	return table;
+}
