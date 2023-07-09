514faa5b8b114d59d4c5623c2ab64ccfd364a7c4
ifiok1equere <equereifiok1@gmail.com>
2023-07-09 23:50:46 +0100

    task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,79 +1,34 @@
 #include "hash_tables.h"
 /**
  * hash_table_print - Function prints a hash table
- *
  * @ht: Hash Table.
  * Return: node for succes, NULL for failure.
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int index = 0, i = 0;
+	unsigned long int index;
 	hash_node_t *ptr;
+	unsigned long int first_node = 1;
 
 	if (!ht || !ht->array)
 	{
-		printf("{}");
+		printf("{}\n");
 		return;
 	}
 	printf("{");
-	while (index < ht->size)
+
+	for (index = 0; index < ht->size; index++)
 	{
 		ptr = ht->array[index];
-		if (ptr != NULL)
+
+		while (ptr != NULL)
 		{
-			if (ptr->next == NULL)
-			{
-				index++;
-				printf("'%s': '%s'", ptr->key, ptr->value);
-				for (i = 0; index < ht->size; i++)
-				{
-					if (ht->array[index] == NULL)
-					{
-						index++;
-						continue;
-					}
-					else
-					{
-						printf(", ");
-						index--;
-						break;
-					}
-				}
-			}
-			else
-			{
-				index++;
-				while (ptr != NULL)
-				{
-					printf("'%s': '%s'", ptr->key, ptr->value);
-					if (ptr->next != NULL)
-					{
-						printf(", ");
-						ptr = ptr->next;
-						continue;
-					}
-					else
-					{
-						for (i = 0; index < ht->size; i++)
-						{
-							if (ht->array[index] == NULL)
-							{
-								index++;
-								continue;
-							}
-							else
-							{
-								printf(", ");
-								index--;
-								break;
-							}
-						}
-					}
-					break;
-				}
-			}
+			if (!first_node)
+				printf(", ");
+			printf("'%s': '%s'", ptr->key, ptr->value);
+			first_node = 0;
+			ptr = ptr->next;
 		}
-		index++;
 	}
 	printf("}\n");
 }
