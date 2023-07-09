12540bfb076d84959c45abbbc1f076cc546a38ba
ifiok1equere <equereifiok1@gmail.com>
2023-07-09 16:46:04 +0100

    task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - Function retrives the value of a key in a key/value pair
+ * node
+ *
+ * @ht: Hash Table.
+ * @key: Hash Table key.
+ * Return: node for succes, NULL for failure.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *ptr;
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	ptr = ht->array[index];
+	if (ptr != NULL)
+	{
+		if (ptr->next == NULL)
+			return (ptr->value);
+		while (ptr != NULL)
+		{
+			if (strcmp(ptr->key, key) == 0)
+				return (ptr->value);
+			ptr = ptr->next;
+		}
+	}
+	return (NULL);
+}
