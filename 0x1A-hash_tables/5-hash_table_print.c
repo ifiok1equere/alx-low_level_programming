6c5d510b7bf7a2df20c374973fe0879715d9bcaf
ifiok1equere <equereifiok1@gmail.com>
2023-07-10 00:01:51 +0100

    task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -8,26 +8,41 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int index;
 	hash_node_t *ptr;
-	unsigned long int first_node = 1;
 
 	if (!ht || !ht->array)
-	{
-		printf("{}\n");
 		return;
-	}
 	printf("{");
-
 	for (index = 0; index < ht->size; index++)
 	{
 		ptr = ht->array[index];
-
 		while (ptr != NULL)
 		{
-			if (!first_node)
-				printf(", ");
 			printf("'%s': '%s'", ptr->key, ptr->value);
-			first_node = 0;
-			ptr = ptr->next;
+			if (ptr->next != NULL)
+			{
+				printf(", ");
+				ptr = ptr->next;
+				continue;
+			}
+			else
+			{
+				index++;
+				while (index < ht->size)
+				{
+					if (ht->array[index] == NULL)
+					{
+						index++;
+						continue;
+					}
+					else
+					{
+						printf(", ");
+						index--;
+						break;
+					}
+				}
+			}
+			break;
 		}
 	}
 	printf("}\n");
