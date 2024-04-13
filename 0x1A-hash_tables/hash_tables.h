d61c5abc537bedaeb78d15d299c7ca4df0125ab0
ifiok1equere <equereifiok1@gmail.com>
2024-04-13 19:23:06 +0100

    add double inclusion

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,4 +1,7 @@
 
+#ifndef
+#define HASH_TABLE_H
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -40,3 +43,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+
+#endif /*HASH_TABEL_H*/
