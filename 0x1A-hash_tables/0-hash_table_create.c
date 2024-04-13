965e8c3019a0867ef4d226e29c01432a8dd33821
ifiok1equere <equereifiok1@gmail.com>
2024-04-13 19:39:43 +0100

    correct arguments for calloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -20,7 +20,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->size = size;
-	table->array = calloc(sizeof(hash_node_t), table->size);
+	table->array = calloc(table->size, sizeof(hash_node_t));
 	if (table->array == NULL)
 		return (NULL);
 
